126ebccab3f033187985cd6626c98448c0f48f6a
Jeff Sandberg <paradox460@gmail.com>
2014-03-24 19:57:56 -0600

    Refactored to intelligently scale text in memes

--- lib/memegen.rb
+++ lib/memegen.rb
@@ -1,5 +1,6 @@
 require 'fileutils'
 require 'RMagick'
+require 'pry'
 
 # Class for handling memes
 class Meme
@@ -15,56 +16,21 @@ class Meme
       bottom = (bottom || '').upcase
 
       canvas = Magick::ImageList.new(path)
-      image = canvas.first
 
-      draw = Magick::Draw.new
-      draw.font = File.join(File.dirname(__FILE__), '..', 'fonts', 'Impact.ttf')
-      draw.font_weight = Magick::BoldWeight
-
-      pointsize = image.columns / 5.0
-      stroke_width = pointsize / 30.0
+      caption_meme(top, Magick::NorthGravity, canvas) unless top.empty?
+      caption_meme(bottom, Magick::SouthGravity, canvas) unless bottom.empty?
 
       # Draw watermark
       unless watermark.nil?
-        watermark_draw = draw.dup
-        watermark_draw.annotate(canvas, 0, 0, 0, 0, watermark) do
+        watermark_draw = Magick::Draw.new
+        watermark_draw.annotate(canvas, 0, 0, 0, 0, " #{watermark}") do
           self.font = 'Helvetica'
           self.fill = 'white'
-          self.stroke_antialias(true)
+          self.text_antialias(false)
           self.font_weight = 100
           self.gravity = Magick::SouthEastGravity
           self.pointsize = 10
           self.undercolor = 'hsla(0,0,0,.5)'
-        end
-      end
-
-      # Draw top
-      unless top.empty?
-        scale, spacing, text = scale_text(top)
-        bottom_draw = draw.dup
-        bottom_draw.annotate(canvas, 0, 0, 0, 0, text) do
-          self.interline_spacing = -(pointsize / spacing)
-          self.stroke_antialias(true)
-          self.stroke = 'black'
-          self.fill = 'white'
-          self.gravity = Magick::NorthGravity
-          self.stroke_width = stroke_width * scale
-          self.pointsize = pointsize * scale
-        end
-      end
-
-      # Draw bottom
-      unless bottom.empty?
-        scale, spacing, text = scale_text(bottom)
-        bottom_draw = draw.dup
-        bottom_draw.annotate(canvas, 0, 0, 0, 0, text) do
-          self.interline_spacing = -(pointsize / spacing)
-          self.stroke_antialias(true)
-          self.stroke = 'black'
-          self.fill = 'white'
-          self.gravity = Magick::SouthGravity
-          self.stroke_width = stroke_width * scale
-          self.pointsize = pointsize * scale
         end
       end
 
@@ -75,29 +41,56 @@ class Meme
 
     private
 
-    def word_wrap(text, col = 80)
-      text.gsub(/(.{1,#{col}})( +|$\n?)|(.{1,#{col}})/, "\\1\\3\n")
+    def caption_meme(text, gravity, canvas)
+      # 28 is the biggest pointsize memes dont look like shit on
+      min_pointsize = 28
+      max_pointsize = 128
+      current_pointsize = min_pointsize
+      current_stroke = current_pointsize / 30.0
+
+      max_width = canvas.columns - 20
+      max_height = (canvas.rows / 2) - 20
+
+      draw = Magick::Draw.new
+      draw.font = File.join(File.dirname(__FILE__), '..', 'fonts', 'Impact.ttf')
+      draw.font_weight = Magick::BoldWeight
+      metrics = nil
+
+      # Calculate out the largest pointsize that will fit
+      loop do
+        draw.pointsize = current_pointsize
+        last_metrics = metrics
+        metrics = draw.get_multiline_type_metrics(text)
+
+        if metrics.width + current_stroke > max_width ||
+          metrics.height + current_stroke > max_height ||
+          current_pointsize > max_pointsize
+          if current_pointsize > min_pointsize
+            current_pointsize -= 1
+            current_stroke = current_pointsize / 30.0
+            metrics = last_metrics
+          end
+          break
+        else
+          current_pointsize += 1
+          current_stroke = current_pointsize / 30.0
+        end
+      end
+
+      text = word_wrap(text, 30) if text.length > 30
+
+      draw.annotate(canvas, canvas.columns, canvas.rows - 10, 0, 0, text) do
+        self.stroke_antialias(true)
+        self.stroke = 'black'
+        self.fill = 'white'
+        self.gravity = gravity
+        self.stroke_width = current_stroke
+        self.pointsize = current_pointsize
+      end
     end
 
-    def scale_text(text)
-      text = text.dup
-      if text.length < 10
-        scale = 1.0
-        spacing = 5
-      elsif text.length < 20
-        text = word_wrap(text, 10)
-        scale = 0.7
-        spacing = 5
-      elsif text.length < 25
-        text = word_wrap(text, 18)
-        scale = 0.5
-        spacing = 7
-      else
-        text = word_wrap(text, 28)
-        scale = 0.3
-        spacing = 10
-      end
-      [scale, spacing, text.strip]
+    def word_wrap(text, col = 80)
+      text.gsub(/(.{1,#{col}})( +|$\n?)|(.{1,#{col}})/, "\\1\\3\n")
     end
   end
 end
